465b55e7e8af8b4b8809ebad5158ba0ea1d807fb
ZohaibSattarr <104334029+ZohaibSattarr@users.noreply.github.com>
2022-05-18 03:12:39 -0700

    Update Utilities.dart

--- lib/controllers/Utilities.dart
+++ lib/controllers/Utilities.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../models/courses.dart';
+import '../models/navbar.dart';
+
 class Utilities {
   static Size getSize(BuildContext context) {
     return MediaQuery.of(context).size;
@@ -8,4 +11,12 @@ class Utilities {
   // List<dynamic> data=[];
 
   }
+    static var baseurl="http://192.168.40.207";
+    static String regno='';
+    static String semester='';
+    static List<Navbarmodel> navbarlist=[];
+    static List<Courses> courseslist=[];
+    static String teachername="";
+
+
 }
